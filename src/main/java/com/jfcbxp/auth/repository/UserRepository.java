679e2cc740d806ce1b3820b92cd33ea3adf2e6ff
Joao Felipe <jfcbxp@gmail.com>
2022-05-19 16:54:43 -0300

    interface UserRepository

---
+++ src/main/java/com/jfcbxp/auth/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.jfcbxp.auth.repository;
+
+import com.jfcbxp.auth.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    @Query("SELECT u FROM User u WHERE u.userName = :userName")
+    Optional<User> findByUserName(@Param("userName") String userName);
+}
